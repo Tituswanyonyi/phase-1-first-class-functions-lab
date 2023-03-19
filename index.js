01606c876d08606be8e8e72d6e86dd0d54c99f1f
titus <titus.unavailable@student.moringaschool.com>
2023-03-19 14:25:38 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers = function anonymousFunction([]){
+return (['Wanyonyi', 'Omuse']);
+
+}
+
+const  returnLastTwoDrivers = function anonymousFunction([]){
+    return (['Wilson', 'stella']);
+
+}
+const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(anInteger){
+    return function(fare){
+      return fare*anInteger;
+    };
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+    return driversToReturn(arrayOfDrivers);
+  };
